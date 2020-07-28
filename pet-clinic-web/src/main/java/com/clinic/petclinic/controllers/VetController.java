e3830c3ea3ab013f3e71a5eeddac0dca5d849697
Senad <dellic.senad@gmail.com>
2020-07-28 12:42:09 +0200

    added vets page and controller. Closes #4

--- pet-clinic-web/src/main/java/com/clinic/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/clinic/petclinic/controllers/VetController.java
@@ -1,4 +1,14 @@
 package com.clinic.petclinic.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class VetController {
+
+    @RequestMapping({"", "/", "/vets/vet", "vets/vet.html"})
+    public String listOfVets() {
+
+        return "vets/vet";
+    }
 }
