fd8167a964f3fd238facc9ecd89cbcc7a5d2218e
Senad <dellic.senad@gmail.com>
2020-08-06 17:14:36 +0200

    Added jpa @ for entities

--- pet-clinic-data/src/main/java/com/clinic/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/clinic/petclinic/model/Pet.java
@@ -1,14 +1,29 @@
 package com.clinic.petclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
+    @Column(name = "name")
     private String name;
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+    @Column(name = "birth_day")
     private LocalDate birthDay;
 
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits;
+
     public Pet() {
+        this.visits = new HashSet<>();
     }
 
     public String getName() {
@@ -41,5 +56,13 @@ public class Pet extends BaseEntity {
 
     public void setBirthDay(LocalDate birthDay) {
         this.birthDay = birthDay;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
