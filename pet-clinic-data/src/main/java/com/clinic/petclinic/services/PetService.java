11cc929206e66f0824572fbb8d4c42c6a5f5bbcf
Senad <dellic.senad@gmail.com>
2020-08-26 14:21:07 +0200

    Added forms for pet and visit

--- pet-clinic-data/src/main/java/com/clinic/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/clinic/petclinic/services/PetService.java
@@ -1,6 +1,9 @@
 package com.clinic.petclinic.services;
 
 import com.clinic.petclinic.model.Pet;
+import com.clinic.petclinic.model.PetType;
+
+import java.util.Collection;
 
 
 public interface PetService extends CRUDService<Pet, Integer> {
--- pet-clinic-web/src/main/java/com/clinic/petclinic/controllers/PetController.java
+++ pet-clinic-web/src/main/java/com/clinic/petclinic/controllers/PetController.java
@@ -1,4 +1,46 @@
 package com.clinic.petclinic.controllers;
 
+import com.clinic.petclinic.model.Owner;
+import com.clinic.petclinic.model.PetType;
+import com.clinic.petclinic.services.OwnerService;
+import com.clinic.petclinic.services.PetService;
+import com.clinic.petclinic.services.PetTypeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Collection;
+
+@Controller
+@RequestMapping("/owners/{ownerId}")
 public class PetController {
+    public static final String VIEWS_PETS_CREATE_OR_UPDATE_FROM = "pets/createOrUpdatePetForm";
+
+    private final PetService petService;
+    private final OwnerService ownerService;
+    private final PetTypeService petTypeService;
+
+    public PetController(PetService petService, OwnerService ownerService, PetTypeService petTypeService) {
+        this.petService = petService;
+        this.ownerService = ownerService;
+        this.petTypeService = petTypeService;
+    }
+
+    @ModelAttribute("types")
+    public Collection<PetType> populatePetTypes() {
+        return petTypeService.findAll();
+    }
+
+    @ModelAttribute("owner")
+    public Owner findOwner(@PathVariable("ownerId") Integer ownerId) {
+        return ownerService.findById(ownerId);
+    }
+
+    @InitBinder("owner")
+    public void initOwnerBinder(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
 }
