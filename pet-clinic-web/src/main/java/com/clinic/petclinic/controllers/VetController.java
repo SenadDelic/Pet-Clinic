fc0159df764996d91a0c13cc175bb4f3ddfb616d
Senad <dellic.senad@gmail.com>
2020-09-03 17:24:07 +0200

    Hope everything is ok

--- pet-clinic-web/src/main/java/com/clinic/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/clinic/petclinic/controllers/VetController.java
@@ -1,11 +1,15 @@
 package com.clinic.petclinic.controllers;
 
+import com.clinic.petclinic.model.Vet;
 import com.clinic.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-// @RequestMapping("/vets")
+import java.util.Set;
+
 @Controller
 public class VetController {
     private final VetService vetService;
@@ -19,5 +23,12 @@ public class VetController {
         model.addAttribute("vets", vetService.findAll());
 
         return "vets/vet";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody
+    Set<Vet> getVetsJson() {
+
+        return vetService.findAll();
     }
 }
