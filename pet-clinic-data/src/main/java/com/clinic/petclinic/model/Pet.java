f508361067cad82a8b23fd08b3820af10a38a4bf
Senad <dellic.senad@gmail.com>
2020-09-21 16:08:50 +0200

    Fixed pet birthday bug

--- pet-clinic-data/src/main/java/com/clinic/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/clinic/petclinic/model/Pet.java
@@ -1,6 +1,7 @@
 package com.clinic.petclinic.model;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -23,6 +24,7 @@ public class Pet extends BaseEntity {
     @JoinColumn(name = "owner_id")
     private Owner owner;
     @Column(name = "birth_day")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthDay;
 
     @Builder
